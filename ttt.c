9e43c6c8154127147d6b7a225c2c78dba29b7e1a
Dany-Chucri <125823060+Dany-Chucri@users.noreply.github.com>
2023-04-21 23:37:55 -0400

    Update ttt.c

--- ttt.c
+++ ttt.c
@@ -1,38 +1,78 @@
-// THIS FILE IMPLEMENTS THE GAME CLIENT
 #define _POSIX_C_SOURCE 200809L
-//PORTNUM is 3360
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <string.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <errno.h>
+
+// ALL BASED ON MENNY'S XMIT
+#define BUFLEN 256
+
+int connect_inet(char *host, char *service)
+{
+    struct addrinfo hints, *info_list, *info;
+    int sock, error;
+
+    // look up remote host
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC; // in practice, this means give us IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // indicate we want a streaming socket
+
+    error = getaddrinfo(host, service, &hints, &info_list);
+    if (error) {
+        fprintf(stderr, "error looking up %s:%s: %s\n", host, service,
+        gai_strerror(error));
+        return -1;
+    }
+
+    for (info = info_list; info != NULL; info = info->ai_next) {
+        sock = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+        if (sock < 0) continue;
+
+        error = connect(sock, info->ai_addr, info->ai_addrlen);
+        if (error) {
+            close(sock);
+            continue;
+        }
+
+        break;
+    }
+    freeaddrinfo(info_list);
+
+    if (info == NULL) {
+        fprintf(stderr, "Unable to connect to %s:%s\n", host, service);
+        return -1;
+    }
+
+    return sock;
+}
 
 int main(int argc, char** argv) {
-    //ip = argv[1];
-    //printf("%s\n", ip);
-    int port = 74920; //int port = 3360;
+    int sock, bytes;
+    char buf[BUFLEN];
 
-    int sock;
-    struct sockaddr_in addr;
-    socklen_t addr_size;
-    char buffer[256];
-    int n;
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("Socket Error\n");
-        exit(1);
+    if (argc != 3) {
+        printf("Specify host and service\n");
+        exit(EXIT_FAILURE);
     }
-    printf("Socket Created\n");
 
-    memset(&addr, '\0', sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = port;
-    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    sock = connect_inet(argv[1], argv[2]);
+    if (sock < 0) exit(EXIT_FAILURE);
+
+    while ((bytes = read(STDIN_FILENO, buf, BUFLEN)) > 0) {
+        int written = write(sock, buf, bytes);
+        if (written < 0) {
+            printf("Could not write to socket!\n");
+        }
+    }
+
+    close(sock);
     
-    if(connect(sock, (struct sockaddr*)&addr, sizeof(addr)) != -1) printf("Connected to server\n");
-    
-    return 0;
+    return EXIT_SUCCESS;
 }
